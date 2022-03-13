8c461838e4be8a1ee39c1a85885de27189421a22
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-03-13 12:06:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.19%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n)
+    int hammingWeight(uint32_t n) 
     {
-         // base case
-    if (n == 0)
-        return 0;
- 
-    else
- 
-        // if last bit set add 1 else add 0
-        return (n & 1) + hammingWeight(n >> 1);
+        unsigned int count = 0;
+        while (n)
+        {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
     }
 };
