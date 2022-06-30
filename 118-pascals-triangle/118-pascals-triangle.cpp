5689b23925b0cec092b6091a894a2ee1844759a4
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-06-30 12:58:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (70.81%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,17 +1,13 @@
-class Solution 
+class Solution {
+public:
+    vector<vector<int>> generate(int n)
     {
-    vector<vector<int>>ans;
-public:    
-    vector<vector<int>>& generate(int n) 
-    {
-        if(n) 
-        {
-            generate(n-1);                       // generate above row first
-            ans.emplace_back(vector<int>(n,1));  // insert current row into triangle
-            for(int i = 1; i < n-1; i++)         // update current row values using above row
-                ans[n-1][i] = ans[n-2][i] + ans[n-2][i-1];    
+        vector<vector<int>> ans(n);             // initialize n rows
+        for(int i = 0; i < n; i++) {
+            ans[i] = vector<int>(i+1,1);        // ith row(0-indexed) has i+1 elements
+            for(int j = 1; j < i; j++)          // 1st and last elements will be 1, rest will be sum of two elements from above row
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];            
         }
         return ans;
-    
     }
 };
