b448b5ae1f6ebf77fa05dfca3f2a4f5595648527
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2023-01-19 17:29:33 +0530

    Time: 42 ms (96.22%), Space: 30.2 MB (75.97%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,13 +1,41 @@
 class Solution {
 public:
-    int subarraysDivByK(vector<int>& A, int K) {
+    int subarraysDivByK(vector<int>& nums, int K) 
+    {
         vector<int> count(K);
         count[0] = 1;
         int prefix = 0, res = 0;
-        for (int a : A) {
+        for (int a : nums) 
+        {
             prefix = (prefix + a % K + K) % K;
             res += count[prefix]++;
         }
         return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Calculate the prefix sum and count it.
+// In c++ and java, a % K + K takes care of the cases where a < 0
