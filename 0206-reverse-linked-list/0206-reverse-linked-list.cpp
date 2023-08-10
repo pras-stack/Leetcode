f95c8614bc77bd7aafa4933e084abdb7ab04b3a2
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2023-08-10 13:34:11 +0530

    Time: 4 ms (71.90%), Space: 8.5 MB (13.35%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -12,26 +12,28 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) 
     {
-        stack<ListNode*>st;
+        stack<ListNode*>st;  //coz we are pushing node's addresses here
         
         ListNode *curr = head;
         
         while(curr!=NULL)
         {
             st.push(curr);
-            curr= curr->next;
+            curr= curr->next; //move to next node
         }
         
-        ListNode *dummyHead = new ListNode(0);
-        curr= dummyHead;
-        
-        while(!st.empty())
-        {
-            curr->next= st.top();
-            curr= curr->next;
-            st.pop();
-        }
-        curr->next = NULL;
-        return dummyHead->next;
+        // Create a dummyHead to build the reversed list
+    ListNode* dummyHead = new ListNode(0);  // Create a dummy node to serve as the head of the reversed list
+    curr = dummyHead;  // Initialize a current pointer for the reversed list
+    
+    // Pop nodes from the stack to build the reversed list
+    while (!st.empty()) {
+        curr->next = st.top();  // Connect the current node to the top node of the stack
+        curr = curr->next;  // Move the current pointer to the newly added node
+        st.pop();  // Pop the top node from the stack
+    }
+    
+    curr->next = NULL;  // Set the next of the last node to NULL
+    return dummyHead->next;  // Return the head of the reversed list
     }
 };
